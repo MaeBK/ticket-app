157947bbddda3fec318476cbc1610e49cc3a1a5c
MaeBk <maeghnabk@gmail.com>
2025-08-09 17:51:20 -0400

    Starting UI

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            'Ticket App',
+                style: TextStyle(
+            color: Colors.deepPurple,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+                ),
+        ),
+        centerTitle: true,
+        actions: [
+          Container(
+            margin: EdgeInsets.all(10),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Color(0xffF7F8F8),
+              borderRadius: BorderRadius.circular(25)
+            ),
+            child: SvgPicture.asset(
+              'assets/icons/avatar default.svg'
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
